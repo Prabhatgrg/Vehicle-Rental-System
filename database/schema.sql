e39a7dbf4b3a64d7b587b9acc940de4b87221490
Prabhatgrg <prabhatgurung34@gmail.com>
2023-04-15 12:33:50 +0545

    Made changes to the schema file and user_handler file

--- database/schema.sql
+++ database/schema.sql
@@ -9,8 +9,8 @@ CREATE TABLE IF NOT EXISTS vehicles(
 
 CREATE TABLE IF NOT EXISTS customers(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     email VARCHAR(100) NOT NULL,
     phone VARCHAR(10) NOT NULL
 );
