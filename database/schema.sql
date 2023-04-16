f0ad65cc53b0ac94d4cf7ad2f0c5dd3ac9059e7c
Prabhatgrg <prabhatgurung34@gmail.com>
2023-04-16 22:39:26 +0545

    Updated schema file

--- database/schema.sql
+++ database/schema.sql
@@ -12,7 +12,8 @@ CREATE TABLE IF NOT EXISTS customers(
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    phone VARCHAR(10) NOT NULL
+    phone VARCHAR(10) NOT NULL,
+    date DATE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS bookings(
