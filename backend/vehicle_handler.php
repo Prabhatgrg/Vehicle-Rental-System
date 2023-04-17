517d49ce608c666eb8d948efb79359c76373ed3f
Prabhatgrg <prabhatgurung34@gmail.com>
2023-04-17 22:59:14 +0545

    Added into vehicle_handler

--- backend/vehicle_handler.php
+++ backend/vehicle_handler.php
@@ -1,4 +1,24 @@
 <?php 
     //Connecting to the database
     require_once './database/db_config.php';
+    if($_SERVER['REQUEST_METHOD']==='POST'){
+        $make = $_POST['make'];
+        $model = $_POST['model'];
+        $year = $_POST['year'];
+        $rate = $_POST['rate'];
+        $availability = $_POST['availability'];
+
+        //Insert the data into the database
+        $stmt = $conn->prepare('INSERT INTO vehicles(make, model, year, rate, availability)VALUES(?, ?, ?, ?, ?)');
+        $stmt->bind_param("ssidi", $make, $model, $year, $rate, $availability);
+        if($stmt->execute()){
+            echo "Vehicle Registered Succesfully";
+        }else{
+            echo "Error occured during the registration";
+        }
+        $stmt->close();
+        $conn->close();
+    }else{
+        echo "Check Method!!";
+    }
 ?>
