371ae652d06912f158b2b729b8fcfff103fc1b20
Prabhatgrg <prabhatgurung34@gmail.com>
2023-05-01 23:12:46 +0545

    Added profile image need more work

--- frontend/profile.php
+++ frontend/profile.php
@@ -5,10 +5,26 @@ get_header()
 <aside class="user-info">
     <div class="container">
         <div class="flex col-4">
-            
+            <div class="user-image">
+            </div>
         </div>
     </div>
 </aside>
+
+<div class="container">
+    <div class="flex col-4">
+        <aside class="user-info">
+            <div class="user-image col-3">
+                <img src="<?php echo get_theme_directory_uri();?>/assets/img/png/default-user.png" alt="Profile Image">
+            </div>
+        </aside>
+    </div>
+    <div class="flex col-7">
+        <section class="listing">
+            Listings
+        </section>
+    </div>
+</div>
 
 <?php
 get_footer()
