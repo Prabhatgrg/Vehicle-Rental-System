732bb84439f25a858bf4bce3f0d7fd7d5760b62e
Neer-Shrestha <shrestha.neer@hotmail.com>
2023-05-01 22:28:35 +0545

    modal

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,13 +1,42 @@
 window.addEventListener("DOMContentLoaded", (event) => {
+    /*
+     * Dropdown menu
+     * selecting the element that contains the class dropdown container
+     */
     const dropdownContainers = document.querySelectorAll(".dropdown-container");
     if (dropdownContainers[0]) {
+        // Looping through the each element
         dropdownContainers.forEach((container) => {
+            // selecting the neccessary elements
             const btnDropdown = container.querySelector(".btn-dropdown");
             const dropdownContent = container.querySelector(".dropdown-content");
 
+            // Listening on click event
             btnDropdown.addEventListener("click", () => {
-                console.log("click");
                 dropdownContent.classList.toggle("show");
+            });
+        });
+    }
+
+    /*
+     * Dropdown menu
+     * selecting the element that contains the class dropdown container
+     */
+    const modalContainers = document.querySelectorAll(".modal-container");
+    if (modalContainers[0]) {
+        // Looping through the each element
+        modalContainers.forEach((modal) => {
+            // selecting the neccessary elements
+            const btnModal = modal.querySelector(".btn-modal");
+            const btnClose = modal.querySelector(".btn-close");
+            const modalContent = modal.querySelector(".modal-content");
+
+            // Listening on click event
+            btnModal.addEventListener("click", () => {
+                modalContent.classList.add("show");
+            });
+            btnClose.addEventListener("click", () => {
+                modalContent.classList.remove("show");
             });
         });
     }
