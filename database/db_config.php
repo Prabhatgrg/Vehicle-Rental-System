38563ba5c2dff33e8f081b2800ed76b13ca70b19
Prabhatgrg <prabhatgurung34@gmail.com>
2023-05-01 11:30:38 +0545

    Added Latest Post Section

--- database/db_config.php
+++ database/db_config.php
@@ -1,20 +1,8 @@
 <?php
-// $servername = 'localhost';
-// $username =  'root';
-// $password = '';
-// $database =  'VRMS';
-
-
-// function connect_to_db()
-// {
-//Create Connection
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-// return $conn;
-//Check Connection
-if ($conn->connect_error) {
-    die("Connection Failed: " . $conn->connect_error);
-} 
-// else {
-//     echo "Connection Success";
-// }
-// }
+    //Create Connection
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    //Check Connection
+    if ($conn->connect_error) {
+        die("Connection Failed: " . $conn->connect_error);
+    }
+?> 
--- frontend/index.php
+++ frontend/index.php
@@ -2,10 +2,10 @@
 
 <?php
 session_start();
-if (isset($_SESSION['success'])) :
+if (isset($_SESSION['success'])):
     $message = $_SESSION['success'];
     unset($_SESSION['success']);
-?>
+    ?>
     <div class="alert">
         <div class="container">
             <p class="bg-success p-2">
@@ -13,11 +13,11 @@ if (isset($_SESSION['success'])) :
             </p>
         </div>
     </div>
-<?php
+    <?php
 endif;
 ?>
 
-<section class="py-4">
+<section class="categories py-4">
     <div class="container">
         <div class="categories">
             <h2 class="category-title h5">Our categories</h2>
@@ -28,6 +28,7 @@ endif;
         </div>
     </div>
 </section>
+
 <section class="trending pb-3">
     <div class="container">
         <h2 class="h5 mb-2">Trending</h2>
@@ -35,13 +36,15 @@ endif;
             <div class="card">
                 <figure class="card-img">
                     <a href="#">
-                        <img src="<?php echo get_theme_directory_uri(); ?>/assets/img/jpg/default-image.jpg" alt="Default Image">
+                        <img src="<?php echo get_theme_directory_uri(); ?>/assets/img/jpg/default-image.jpg"
+                            alt="Default Image">
                     </a>
                 </figure>
                 <ul class="card-features">
                     <li>
                         <a href="#">
-                            <svg width="125" height="185" viewBox="0 0 125 185" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg width="125" height="185" viewBox="0 0 125 185" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
                                 <path d="M5 175V5H120V175L61 138L5 175Z" stroke="black" stroke-width="10" />
                             </svg>
                         </a>
@@ -49,19 +52,21 @@ endif;
                 </ul>
                 <div class="card-body pt-1">
                     <h3 class="card-title h5"><a href="#">Car in rent</a></h3>
-                    <span>Rs. 5,000/day</span>
+                    <span class="price">Rs. 5,000/day</span>
                 </div>
             </div>
             <div class="card">
                 <figure class="card-img">
                     <a href="#">
-                        <img src="<?php echo get_theme_directory_uri(); ?>/assets/img/jpg/default-image.jpg" alt="Default Image">
+                        <img src="<?php echo get_theme_directory_uri(); ?>/assets/img/jpg/default-image.jpg"
+                            alt="Default Image">
                     </a>
                 </figure>
                 <ul class="card-features">
                     <li>
                         <a href="#">
-                            <svg width="125" height="185" viewBox="0 0 125 185" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg width="125" height="185" viewBox="0 0 125 185" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
                                 <path d="M5 175V5H120V175L61 138L5 175Z" stroke="black" stroke-width="10" />
                             </svg>
                         </a>
@@ -69,19 +74,21 @@ endif;
                 </ul>
                 <div class="card-body pt-1">
                     <h3 class="card-title h5"><a href="#">Car in rent</a></h3>
-                    <span>Rs. 5,000/day</span>
+                    <span class="price">Rs. 5,000/day</span>
                 </div>
             </div>
             <div class="card">
                 <figure class="card-img">
                     <a href="#">
-                        <img src="<?php echo get_theme_directory_uri(); ?>/assets/img/jpg/default-image.jpg" alt="Default Image">
+                        <img src="<?php echo get_theme_directory_uri(); ?>/assets/img/jpg/default-image.jpg"
+                            alt="Default Image">
                     </a>
                 </figure>
                 <ul class="card-features">
                     <li>
                         <a href="#">
-                            <svg width="125" height="185" viewBox="0 0 125 185" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg width="125" height="185" viewBox="0 0 125 185" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
                                 <path d="M5 175V5H120V175L61 138L5 175Z" stroke="black" stroke-width="10" />
                             </svg>
                         </a>
@@ -89,19 +96,21 @@ endif;
                 </ul>
                 <div class="card-body pt-1">
                     <h3 class="card-title h5"><a href="#">Car in rent</a></h3>
-                    <span>Rs. 5,000/day</span>
+                    <span class="price">Rs. 5,000/day</span>
                 </div>
             </div>
             <div class="card">
                 <figure class="card-img">
                     <a href="#">
-                        <img src="<?php echo get_theme_directory_uri(); ?>/assets/img/jpg/default-image.jpg" alt="Default Image">
+                        <img src="<?php echo get_theme_directory_uri(); ?>/assets/img/jpg/default-image.jpg"
+                            alt="Default Image">
                     </a>
                 </figure>
                 <ul class="card-features">
                     <li>
                         <a href="#">
-                            <svg width="125" height="185" viewBox="0 0 125 185" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg width="125" height="185" viewBox="0 0 125 185" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
                                 <path d="M5 175V5H120V175L61 138L5 175Z" stroke="black" stroke-width="10" />
                             </svg>
                         </a>
@@ -109,19 +118,21 @@ endif;
                 </ul>
                 <div class="card-body pt-1">
                     <h3 class="card-title h5"><a href="#">Car in rent</a></h3>
-                    <span>Rs. 5,000/day</span>
+                    <span class="price">Rs. 5,000/day</span>
                 </div>
             </div>
             <div class="card">
                 <figure class="card-img">
                     <a href="#">
-                        <img src="<?php echo get_theme_directory_uri(); ?>/assets/img/jpg/default-image.jpg" alt="Default Image">
+                        <img src="<?php echo get_theme_directory_uri(); ?>/assets/img/jpg/default-image.jpg"
+                            alt="Default Image">
                     </a>
                 </figure>
                 <ul class="card-features">
                     <li>
                         <a href="#">
-                            <svg width="125" height="185" viewBox="0 0 125 185" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg width="125" height="185" viewBox="0 0 125 185" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
                                 <path d="M5 175V5H120V175L61 138L5 175Z" stroke="black" stroke-width="10" />
                             </svg>
                         </a>
@@ -129,13 +140,43 @@ endif;
                 </ul>
                 <div class="card-body pt-1">
                     <h3 class="card-title h5"><a href="#">Car in rent</a></h3>
-                    <span>Rs. 5,000/day</span>
+                    <span class="price">Rs. 5,000/day</span>
                 </div>
             </div>
         </div>
     </div>
 </section>
 
+<section class="latest pb-3">
+    <div class="container">
+        <h2 class="h5 mb-2">Latest Post</h2>
+        <div class="grid gap-2">
+            <div class="card-linear">
+                <figure class="card-img">
+                    <a href="#">
+                        <img src="<?php echo get_theme_directory_uri(); ?>/assets/img/jpg/default-image.jpg"
+                            alt="Default Image">
+                    </a>
+                </figure>
+                <div class="card-body flex-1 p-2">
+                    <h3 class="card-title h5 mb-3"><a href="#"> New Car</a></h3>
+                    <div class="product-description mb-3">
+                        <p>Vehicle Description Here</p>
+                    </div>
+
+                    <div class="price-and-availability flex-1 gap-2 mb-3">
+                        <span class="price">Rs. 3000/day</span>
+                        <span class="available">| Available: </span>
+                    </div>
+                    <div class="location-and-time flex justify-content-between">
+                        <span clas="location">Naikap, Kathmandu</span>
+                        <span class="time">Just Now</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php
 get_footer();
