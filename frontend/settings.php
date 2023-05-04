0607e17f9d3ab4b2c5d4a2ef55137e57050fb1a7
Neer-Shrestha <shrestha.neer@hotmail.com>
2023-05-04 20:00:43 +0545

    settings update

--- frontend/settings.php
+++ frontend/settings.php
@@ -2,21 +2,27 @@
 get_header("Settings");
 ?>
 
-<section class="update-password">
+<section class="update-password py-5">
     <div class="container">
         <div class="flex justify-content-center align-items-center">
-            <form action="#" method="POST">
-                <div class="form-floating">
-                    <span>Update Password</span>
-                    <input type="text" name="oldPassword" id="oldPassword" class="form-control" placeholder="Old Password">
-                    <label for="oldPassword">Old Password</label>
-                    <input type="password" name="newPassword" id="newPassword" class="form-control" placeholder="New Password">
-                    <label for="newPassword">New Password</label>
-                    <input type="text" name="confirmPassword" id="confirmPassword" class="form-control" placeholder="Confirm Password">
-                    <label for="confirmPassword">Confirm Password</label>
-                    <button>Change Password</button>
-                </div>
-            </form>
+            <div class="col-md-5 col-lg-4">
+                <form action="#" method="POST" class="grid gap-1">
+                    <h2 class="h3">Update Password</h2>
+                    <div class="form-floating">
+                        <input type="text" name="oldPassword" id="oldPassword" class="form-control" placeholder="Old Password">
+                        <label for="oldPassword">Old Password</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" name="newPassword" id="newPassword" class="form-control" placeholder="New Password">
+                        <label for="newPassword">New Password</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="text" name="confirmPassword" id="confirmPassword" class="form-control" placeholder="Confirm Password">
+                        <label for="confirmPassword">Confirm Password</label>
+                    </div>
+                    <button class="btn btn-dark">Change Password</button>
+                </form>
+            </div>
         </div>
     </div>
 </section>
