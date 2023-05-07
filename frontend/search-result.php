41f2e90ae9f4f8214689d671a675c5ca1abb6132
Prabhatgrg <prabhatgurung34@gmail.com>
2023-05-07 22:05:04 +0545

    Search Result Start

---
+++ frontend/search-result.php
@@ -0,0 +1,17 @@
+<?php
+get_header("Search Result");
+?>
+
+<div class="container-search-page py-2">
+    <div class="search-section-body">
+        <section class="col-6 search-result">
+            
+        </section>
+    </div>
+</div>
+
+
+
+<?php
+get_footer();
+?>
