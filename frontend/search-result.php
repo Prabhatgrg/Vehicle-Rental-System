34d5f01ba660e4927d5a0716c5b4b43e88f931f3
Prabhatgrg <prabhatgurung34@gmail.com>
2023-05-08 21:16:46 +0545

    Container Class Added for padding

--- frontend/search-result.php
+++ frontend/search-result.php
@@ -9,152 +9,154 @@ get_header("Search Result");
                 <h3>Search Result for: </h3>
                 <span class="h3">Result</span>
             </div>
-            <div class="result-list">
-                <div class="card-linear">
-                    <figure class="card-img">
-                        <a href="#">
-                            <img src="<?php echo get_theme_directory_uri(); ?>/assets/img/jpg/default-image.jpg" alt="Default Image">
-                        </a>
-                    </figure>
+            <div class="container">
+                <div class="result-list">
+                    <div class="card-linear">
+                        <figure class="card-img">
+                            <a href="#">
+                                <img src="<?php echo get_theme_directory_uri(); ?>/assets/img/jpg/default-image.jpg" alt="Default Image">
+                            </a>
+                        </figure>
 
-                    <div class="card-body flex-1 p-2">
+                        <div class="card-body flex-1 p-2">
 
-                        <div class="flex">
-                            <h3 class="card-title flex-1 h5 mb-3"><a href="#"> New Car</a></h3>
+                            <div class="flex">
+                                <h3 class="card-title flex-1 h5 mb-3"><a href="#"> New Car</a></h3>
 
-                            <ul class="card-features">
-                                <li>
-                                    <a href="#">
-                                        <svg width="125" height="185" viewBox="0 0 125 185" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M5 175V5H120V175L61 138L5 175Z" stroke="black" stroke-width="10" />
-                                        </svg>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
+                                <ul class="card-features">
+                                    <li>
+                                        <a href="#">
+                                            <svg width="125" height="185" viewBox="0 0 125 185" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M5 175V5H120V175L61 138L5 175Z" stroke="black" stroke-width="10" />
+                                            </svg>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
 
-                        <div class="product-description mb-3">
-                            <p>Vehicle Description Here</p>
-                        </div>
+                            <div class="product-description mb-3">
+                                <p>Vehicle Description Here</p>
+                            </div>
 
-                        <div class="price-and-availability flex-1 gap-2 mb-3">
-                            <span class="price">Rs. 3000/day</span>
-                            <span class="available">| Available: </span>
-                        </div>
-                        <div class="location-and-time flex justify-content-between">
-                            <span clas="location">Naikap, Kathmandu</span>
-                            <span class="time">Just Now</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-linear">
-                    <figure class="card-img">
-                        <a href="#">
-                            <img src="<?php echo get_theme_directory_uri(); ?>/assets/img/jpg/default-image.jpg" alt="Default Image">
-                        </a>
-                    </figure>
-
-                    <div class="card-body flex-1 p-2">
-
-                        <div class="flex">
-                            <h3 class="card-title flex-1 h5 mb-3"><a href="#"> New Car</a></h3>
-
-                            <ul class="card-features">
-                                <li>
-                                    <a href="#">
-                                        <svg width="125" height="185" viewBox="0 0 125 185" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M5 175V5H120V175L61 138L5 175Z" stroke="black" stroke-width="10" />
-                                        </svg>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div class="product-description mb-3">
-                            <p>Vehicle Description Here</p>
-                        </div>
-
-                        <div class="price-and-availability flex-1 gap-2 mb-3">
-                            <span class="price">Rs. 3000/day</span>
-                            <span class="available">| Available: </span>
-                        </div>
-                        <div class="location-and-time flex justify-content-between">
-                            <span clas="location">Naikap, Kathmandu</span>
-                            <span class="time">Just Now</span>
+                            <div class="price-and-availability flex-1 gap-2 mb-3">
+                                <span class="price">Rs. 3000/day</span>
+                                <span class="available">| Available: </span>
+                            </div>
+                            <div class="location-and-time flex justify-content-between">
+                                <span clas="location">Naikap, Kathmandu</span>
+                                <span class="time">Just Now</span>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="card-linear">
-                    <figure class="card-img">
-                        <a href="#">
-                            <img src="<?php echo get_theme_directory_uri(); ?>/assets/img/jpg/default-image.jpg" alt="Default Image">
-                        </a>
-                    </figure>
+                    <div class="card-linear">
+                        <figure class="card-img">
+                            <a href="#">
+                                <img src="<?php echo get_theme_directory_uri(); ?>/assets/img/jpg/default-image.jpg" alt="Default Image">
+                            </a>
+                        </figure>
 
-                    <div class="card-body flex-1 p-2">
+                        <div class="card-body flex-1 p-2">
 
-                        <div class="flex">
-                            <h3 class="card-title flex-1 h5 mb-3"><a href="#"> New Car</a></h3>
+                            <div class="flex">
+                                <h3 class="card-title flex-1 h5 mb-3"><a href="#"> New Car</a></h3>
 
-                            <ul class="card-features">
-                                <li>
-                                    <a href="#">
-                                        <svg width="125" height="185" viewBox="0 0 125 185" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M5 175V5H120V175L61 138L5 175Z" stroke="black" stroke-width="10" />
-                                        </svg>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
+                                <ul class="card-features">
+                                    <li>
+                                        <a href="#">
+                                            <svg width="125" height="185" viewBox="0 0 125 185" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M5 175V5H120V175L61 138L5 175Z" stroke="black" stroke-width="10" />
+                                            </svg>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
 
-                        <div class="product-description mb-3">
-                            <p>Vehicle Description Here</p>
-                        </div>
+                            <div class="product-description mb-3">
+                                <p>Vehicle Description Here</p>
+                            </div>
 
-                        <div class="price-and-availability flex-1 gap-2 mb-3">
-                            <span class="price">Rs. 3000/day</span>
-                            <span class="available">| Available: </span>
-                        </div>
-                        <div class="location-and-time flex justify-content-between">
-                            <span clas="location">Naikap, Kathmandu</span>
-                            <span class="time">Just Now</span>
+                            <div class="price-and-availability flex-1 gap-2 mb-3">
+                                <span class="price">Rs. 3000/day</span>
+                                <span class="available">| Available: </span>
+                            </div>
+                            <div class="location-and-time flex justify-content-between">
+                                <span clas="location">Naikap, Kathmandu</span>
+                                <span class="time">Just Now</span>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="card-linear">
-                    <figure class="card-img">
-                        <a href="#">
-                            <img src="<?php echo get_theme_directory_uri(); ?>/assets/img/jpg/default-image.jpg" alt="Default Image">
-                        </a>
-                    </figure>
+                    <div class="card-linear">
+                        <figure class="card-img">
+                            <a href="#">
+                                <img src="<?php echo get_theme_directory_uri(); ?>/assets/img/jpg/default-image.jpg" alt="Default Image">
+                            </a>
+                        </figure>
 
-                    <div class="card-body flex-1 p-2">
+                        <div class="card-body flex-1 p-2">
 
-                        <div class="flex">
-                            <h3 class="card-title flex-1 h5 mb-3"><a href="#"> New Car</a></h3>
+                            <div class="flex">
+                                <h3 class="card-title flex-1 h5 mb-3"><a href="#"> New Car</a></h3>
 
-                            <ul class="card-features">
-                                <li>
-                                    <a href="#">
-                                        <svg width="125" height="185" viewBox="0 0 125 185" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M5 175V5H120V175L61 138L5 175Z" stroke="black" stroke-width="10" />
-                                        </svg>
-                                    </a>
-                                </li>
-                            </ul>
+                                <ul class="card-features">
+                                    <li>
+                                        <a href="#">
+                                            <svg width="125" height="185" viewBox="0 0 125 185" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M5 175V5H120V175L61 138L5 175Z" stroke="black" stroke-width="10" />
+                                            </svg>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div class="product-description mb-3">
+                                <p>Vehicle Description Here</p>
+                            </div>
+
+                            <div class="price-and-availability flex-1 gap-2 mb-3">
+                                <span class="price">Rs. 3000/day</span>
+                                <span class="available">| Available: </span>
+                            </div>
+                            <div class="location-and-time flex justify-content-between">
+                                <span clas="location">Naikap, Kathmandu</span>
+                                <span class="time">Just Now</span>
+                            </div>
                         </div>
+                    </div>
+                    <div class="card-linear">
+                        <figure class="card-img">
+                            <a href="#">
+                                <img src="<?php echo get_theme_directory_uri(); ?>/assets/img/jpg/default-image.jpg" alt="Default Image">
+                            </a>
+                        </figure>
 
-                        <div class="product-description mb-3">
-                            <p>Vehicle Description Here</p>
-                        </div>
+                        <div class="card-body flex-1 p-2">
 
-                        <div class="price-and-availability flex-1 gap-2 mb-3">
-                            <span class="price">Rs. 3000/day</span>
-                            <span class="available">| Available: </span>
-                        </div>
-                        <div class="location-and-time flex justify-content-between">
-                            <span clas="location">Naikap, Kathmandu</span>
-                            <span class="time">Just Now</span>
+                            <div class="flex">
+                                <h3 class="card-title flex-1 h5 mb-3"><a href="#"> New Car</a></h3>
+
+                                <ul class="card-features">
+                                    <li>
+                                        <a href="#">
+                                            <svg width="125" height="185" viewBox="0 0 125 185" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M5 175V5H120V175L61 138L5 175Z" stroke="black" stroke-width="10" />
+                                            </svg>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div class="product-description mb-3">
+                                <p>Vehicle Description Here</p>
+                            </div>
+
+                            <div class="price-and-availability flex-1 gap-2 mb-3">
+                                <span class="price">Rs. 3000/day</span>
+                                <span class="available">| Available: </span>
+                            </div>
+                            <div class="location-and-time flex justify-content-between">
+                                <span clas="location">Naikap, Kathmandu</span>
+                                <span class="time">Just Now</span>
+                            </div>
                         </div>
                     </div>
                 </div>
