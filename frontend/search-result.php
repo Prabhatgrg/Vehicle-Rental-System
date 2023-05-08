c1808c3f22a44cdfc42a96eab1a3347e6ddd79ed
Prabhatgrg <prabhatgurung34@gmail.com>
2023-05-08 20:44:13 +0545

    Search Result

--- frontend/search-result.php
+++ frontend/search-result.php
@@ -4,8 +4,11 @@ get_header("Search Result");
 
 <div class="container-search-page py-2">
     <div class="search-section-body">
-        <section class="col-6 search-result">
-            
+        <section class="search-result">
+            <div class="search-title flex justify-content-center gap-1">
+                <h3>Search Result for: </h3>
+                <span class="h3">Result</span>
+            </div>
         </section>
     </div>
 </div>
