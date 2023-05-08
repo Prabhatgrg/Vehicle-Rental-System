9de24b8912be24bf7032b52bda81616af1bf9cdb
Prabhatgrg <prabhatgurung34@gmail.com>
2023-05-08 21:08:26 +0545

    Added Divider for search result and profile

--- frontend/search-result.php
+++ frontend/search-result.php
@@ -5,7 +5,7 @@ get_header("Search Result");
 <div class="container-search-page py-2">
     <div class="search-section-body">
         <section class="search-result">
-            <div class="search-title flex justify-content-center gap-1">
+            <div class="search-title flex justify-content-center gap-1 pb-2">
                 <h3>Search Result for: </h3>
                 <span class="h3">Result</span>
             </div>
