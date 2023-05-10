d4bceaddf5b283422b9f2d61279afdcadf65e44a
Prabhatgrg <prabhatgurung34@gmail.com>
2023-05-10 22:09:36 +0545

    Star Rating Fill

--- frontend/profile.php
+++ frontend/profile.php
@@ -33,7 +33,7 @@ get_header("Profile")
                             <li><button class="tab-button" data-target="#user-post-reviews">Reviews</button></li>
                         </ul>
                         <div class="tab-content">
-                            <div class="tab-pane active" id="user-ad-posts">
+                            <div class="tab-pane" id="user-ad-posts">
                                 <div class="tab-ad-container text-center">
                                     Here Goes the Ad posts that user posted.
                                 </div>
@@ -116,7 +116,7 @@ get_header("Profile")
                                     </div>
                                 </div>
                             </div>
-                            <div class="tab-pane" id="user-post-reviews">
+                            <div class="tab-pane active" id="user-post-reviews">
                                 <!-- <div class="tab-review-container text-center">
                                     Here Goes All the comments that user's posted post has.
                                 </div> -->
@@ -132,7 +132,7 @@ get_header("Profile")
                                         </div>
                                         <div class="star py-1">
                                             <div class="rating">
-                                                <ul class="star-filled">
+                                                <div class="star-filled" style="width:70%">
                                                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                         <path d="M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26Z" fill="black" />
                                                     </svg>
@@ -148,8 +148,8 @@ get_header("Profile")
                                                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                         <path d="M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26Z" fill="black" />
                                                     </svg>
-                                                </ul>
-                                                <ul class="star-outline">
+                                                </div>
+                                                <div class="star-outline">
                                                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                         <path d="M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26ZM12.0006 15.968L16.2473 18.3451L15.2988 13.5717L18.8719 10.2674L14.039 9.69434L12.0006 5.27502L9.96214 9.69434L5.12921 10.2674L8.70231 13.5717L7.75383 18.3451L12.0006 15.968Z" fill="black" />
                                                     </svg>
@@ -165,7 +165,7 @@ get_header("Profile")
                                                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                         <path d="M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26ZM12.0006 15.968L16.2473 18.3451L15.2988 13.5717L18.8719 10.2674L14.039 9.69434L12.0006 5.27502L9.96214 9.69434L5.12921 10.2674L8.70231 13.5717L7.75383 18.3451L12.0006 15.968Z" fill="black" />
                                                     </svg>
-                                                </ul>
+                                                </div>
                                             </div>
                                         </div>
                                         <div class="review-response">
