8ee728329ec0594a5f74fdfa721bf79466649317
Neer-Shrestha <shrestha.neer@hotmail.com>
2023-05-12 09:08:50 +0545

    chat page

--- frontend/page-chat.php
+++ frontend/page-chat.php
@@ -2,14 +2,85 @@
 get_header("Chat");
 ?>
 
-<section class="chat-area">
+<section class="chat-area py-5">
     <div class="container">
         <div class="flex flex-wrap">
             <div class="col-md-4">
+                <h2 class="h3 mb-1">Chat</h2>
+                <ul class="chat-user-list">
+                    <li>
+                        <a href="#" class="chat-item chat-link">
+                            <div class="user-avatar">
+                                <img class="user-image" src="<?php echo get_theme_directory_uri(); ?>/assets/img/png/default-user.png" alt="default user avatar">
+                                <span class="status active">
 
+                                </span>
+                            </div>
+                            <div class="chat-info">
+                                <span class="user-name">Prabhat Gurung</span>
+                                <span class="chat-last-msg">Lorem, ipsum dolor.</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="chat-item chat-link">
+                            <div class="user-avatar">
+                                <img class="user-image" src="<?php echo get_theme_directory_uri(); ?>/assets/img/png/default-user.png" alt="default user avatar">
+                                <span class="status active">
+
+                                </span>
+                            </div>
+                            <div class="chat-info">
+                                <span class="user-name">Prabhat Gurung</span>
+                                <span class="chat-last-msg">Lorem, ipsum dolor.</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="chat-item chat-link">
+                            <div class="user-avatar">
+                                <img class="user-image" src="<?php echo get_theme_directory_uri(); ?>/assets/img/png/default-user.png" alt="default user avatar">
+                                <span class="status active">
+
+                                </span>
+                            </div>
+                            <div class="chat-info">
+                                <span class="user-name">Prabhat Gurung</span>
+                                <span class="chat-last-msg">Lorem, ipsum dolor.</span>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
             </div>
             <div class="col-md-8">
+                <div class="chat-container">
+                    <div class="chat-header">
+                        <div class="chat-item">
+                            <div class="user-avatar">
+                                <img class="user-image" src="<?php echo get_theme_directory_uri(); ?>/assets/img/png/default-user.png" alt="default user avatar">
+                                <span class="status active">
 
+                                </span>
+                            </div>
+                            <div class="chat-info">
+                                <span class="user-name">Prabhat Gurung</span>
+                                <span class="chat-last-msg">Lorem, ipsum dolor.</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="chat-body">
+                        <div class="chat-conversation">
+                            <div class="conv-item">
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="chat-footer">
+                        <form action="#" method="post" class="chat-form">
+
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
