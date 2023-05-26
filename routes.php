ba53af1b06cf181a758c866c11a09a220c34d811
Prabhatgrg <prabhatgurung34@gmail.com>
2023-05-26 08:32:39 +0545

    Dropdown start

--- routes.php
+++ routes.php
@@ -12,7 +12,8 @@ $routes = [
     get_root_directory_uri() . '/settings' => 'frontend/settings.php',
     get_root_directory_uri() . '/chat' => 'frontend/page-chat.php',
     get_root_directory_uri() . '/search' => 'frontend/search-result.php',
-    get_root_directory_uri() . '/admin' => 'backend/admin/admin.php'
+    get_root_directory_uri() . '/admin' => 'backend/admin/admin.php',
+    get_root_directory_uri() . '/editpost' => 'backend/admin/editpost.php'
 ];
 
 // Get the URL path from the request
