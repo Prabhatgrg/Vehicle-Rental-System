7858faa54f6d6028275c15812b020e5a4571196c
Prabhatgrg <prabhatgurung34@gmail.com>
2023-07-07 18:49:08 +0545

    Content-User Admin Started

--- backend/admin/content/content-user.php
+++ backend/admin/content/content-user.php
@@ -8,8 +8,18 @@ global $conn;
 $stmt = $conn->prepare("SELECT user_fullname, user_login, user_email, user_phone, user_date FROM re_users");
 $stmt->execute();
 $result = $stmt->get_result();
-$data = $result->fetch_array();
+$data = $result->fetch_all(MYSQLI_ASSOC);
 
-print_r($data);
-
+// print_r($data);
+foreach($data as $user) :
 ?>
+
+<ul>
+    <li><?php echo $user['user_fullname'];?></li>
+    <li><?php echo $user['user_login'];?></li>
+    <li><?php echo $user['user_email'];?></li>
+    <li><?php echo $user['user_phone'];?></li>
+    <li><?php echo $user['user_date'];?></li>
+</ul>
+
+<?php endforeach; ?>
