b42eb7c42dbdc5a668165691b14cde9d8dde45e6
Neer-Shrestha <shrestha.neer@hotmail.com>
2023-07-27 12:44:07 +0545

    wraped string with function and removed the submit name on search form

--- frontend/action/action_search.php
+++ frontend/action/action_search.php
@@ -1,7 +1,7 @@
 <?php get_header("Search Result");
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') :
-    if (isset($_GET['submit'])) :
+    if (isset($_GET['search'])) :
         $search = $_GET['search'];
         $query = $conn->prepare("SELECT * FROM re_posts WHERE (post_title LIKE '%$search%' OR post_location LIKE '%$search%') AND post_status = 'published'");
         $query->execute();
@@ -14,7 +14,7 @@ endif;
     <div class="container">
         <div class="flex justify-content-center align-items-center py-2">
             <div class="col-md-12 col-lg-9">
-                <h3 class="text-center divider-bottom my-2">Search Result For: <?php echo $_GET['search']; ?></h3>
+                <h3 class="text-center divider-bottom my-2">Search Result For: <?php echo htmlspecialchars($_GET['search']); ?></h3>
                 <?php
                 if ($result->num_rows > 0) : ?>
                     <div class="grid gap-3 gap-md-2">
@@ -44,7 +44,7 @@ endif;
                                 <div class="card-body flex-1 p-2">
 
                                     <div class="flex">
-                                        <h3 class="card-title flex-1 h5 mb-3"><a href="<?php echo get_root_directory_uri() . '/post?id=' . urldecode($row['post_id']); ?>"><?php echo $row['post_title']; ?></a></h3>
+                                        <h3 class="card-title flex-1 h5 mb-3"><a href="<?php echo get_root_directory_uri() . '/post?id=' . urldecode($row['post_id']); ?>"><?php echo htmlspecialchars($row['post_title']); ?></a></h3>
 
                                         <?php if (is_login()) :
 
@@ -64,16 +64,16 @@ endif;
                                     </div>
 
                                     <div class="product-description mb-3">
-                                        <p><?php echo $row['post_description']; ?></p>
+                                        <p><?php echo htmlspecialchars($row['post_description']); ?></p>
                                     </div>
 
                                     <div class="price-and-availability flex-1 gap-2 mb-3">
-                                        <span class="price">Rs <?php echo $row['post_price']; ?> /day</span>
+                                        <span class="price">Rs <?php echo htmlspecialchars($row['post_price']); ?> /day</span>
                                         <span class="available">| Available: </span>
                                     </div>
                                     <div class="location-and-time flex justify-content-between">
-                                        <span class="location"><?php echo $row['post_location']; ?></span>
-                                        <span class="time"><?php echo $row['post_date']; ?></span>
+                                        <span class="location"><?php echo htmlspecialchars($row['post_location']); ?></span>
+                                        <span class="time"><?php echo htmlspecialchars($row['post_date']); ?></span>
                                     </div>
                                 </div>
                             </div>
